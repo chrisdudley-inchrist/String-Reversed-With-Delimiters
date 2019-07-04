9a777cb1596d557986e08c45c3c72fb71e810a87
chrisdudley-inchrist <52437944+chrisdudley-inchrist@users.noreply.github.com>
2019-07-04 13:49:49 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 /**
  * Good morning! Here's your coding interview problem for today.
- *
+ * <p>
  * This problem was asked by Facebook.
- *
+ * <p>
  * Given a string and a set of delimiters, reverse the words in the
  * string while maintaining the relative order of the delimiters.
  * For example, given "hello/world:here", return "here/world:hello"
- *
+ * <p>
  * Follow-up: Does your solution work for
  * the following cases: "hello/world:here/", "hello//world:here"
  */
@@ -16,21 +16,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String reversed = stringReverser("hello/world:here/");
+        String reversed = stringReverser("hello//world:here/");
         System.out.println(reversed);
     }
 
     private static String stringReverser(String s) {
         String word = "";
         String reversed = "";
+        int consecutiveDel = 0;
+        String consecDelStr = "";
         LinkedList<String> wordsBackwards = new LinkedList<>();
-        LinkedList<Character> delimiters = new LinkedList<>();
+        LinkedList<String> delimiters = new LinkedList<>();
         for (char c : s.toCharArray()) {
             if (c == '/' || c == ':') {
-                delimiters.addLast(c);
-                wordsBackwards.addFirst(word);
+                consecutiveDel++;
+                consecDelStr += c;
+
+                if (consecutiveDel > 1) {
+                    delimiters.removeLast();
+                }
+
+                delimiters.addLast(consecDelStr);
+                if (word != "") {
+                    wordsBackwards.addFirst(word);
+                }
                 word = "";
+
             } else {
+                consecutiveDel = 0;
+                consecDelStr = "";
                 word += c;
             }
         }
@@ -43,7 +57,6 @@ public class Main {
         if (wordsBackwards.size() < delimiters.size()) {
             biggestSize = delimiters.size();
         }
-
 
         for (int i = 0; i < biggestSize; i++) {
             reversed += wordsBackwards.get(i);
